f017f0c6bd3d14dd3b2712a12b1a36860b7ba1aa
Anshul <anshul.jain@appdirect.com>
2018-11-20 16:58:41 +0530

    CHAP-1: Listener initial commit

---
+++ src/main/java/com/syssolu/rabbitmqlistener/RabbitmqlistenerApplication.java
@@ -0,0 +1,12 @@
+package com.syssolu.rabbitmqlistener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqlistenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqlistenerApplication.class, args);
+	}
+}
